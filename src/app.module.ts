95b2c75501afeeeb5cf0606e6433985772762fe4
Willis "Thiên Phúc" Crympt <phucplaymc@gmail.com>
2025-01-05 20:48:32 +0700

    add CI/CD pipeline, pull request template, and configure environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.env.${process.env.NODE_ENV || 'development'}`,
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
